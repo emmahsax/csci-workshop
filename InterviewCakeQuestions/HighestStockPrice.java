99cf031b7d724c264bb794bd40d581ff4951d300
Emma Sax <emma.the.coder@mail.com>
2016-05-04 10:11:13 -0500

    Updating comments

--- InterviewCakeQuestions/HighestStockPrice.java
+++ InterviewCakeQuestions/HighestStockPrice.java
@@ -2,10 +2,12 @@
  * Suppose we could access yesterday's stock prices as an array, where:
  *  - The indices are the time in minutes past trade opening time, which was 9:30am local time.
  *  - The values are the price in dollars of Apple stock at that time.
- * So if the stock cost $500 at 10:30am, stocks[60] = 500.
- * Write an efficient function that takes stocks and returns the best profit that could be made from 1 purchase and 1 sale of stock.
- * Notes: you must buy before you sell (cannot buy and sell at the same time).
- * Gotchas: it isn't sufficient to just take the difference between the highest and lowest prices (highest price may come before loweset price); if stock value goes down all day, then profit may be negative; this is solvable in O(n) time and O(1) space.
+ * So if the stock cost $500 at 10:30am, stocks[60] = 500. Write an efficient function that takes stocks and
+ * returns the best profit that could be made from 1 purchase and 1 sale of stock.
+ * Notes: you must buy before you sell (cannot buy and sell at the same time)
+ * Gotchas: it isn't sufficient to just take the difference between the highest and lowest prices (highest price may come before loweset price)
+ *          if stock value goes down all day, then profit may be negative
+ *          this is solvable in O(n) time and O(1) space
  */
 
 public class HighestStockPrice {
@@ -19,9 +21,9 @@ public class HighestStockPrice {
 
     for (int i = 1; i < stocks.length; i++) {
       int currentPrice = stocks[i];
-      int potential = currentPrice - minPrice;
-      maxProfit = Math.max(maxProfit, potential);
-      minPrice = Math.min(minPrice,currentPrice);
+      int potentialPrice = currentPrice - minPrice;
+      maxProfit = Math.max(maxProfit, potentialPrice);
+      minPrice = Math.min(minPrice, currentPrice);
     }
 
     return maxProfit;
