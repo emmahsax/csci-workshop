c6661d9cb55134de8ce4cd9229680f402b19cbca
Emma Sax <emma.sax4@gmail.com>
2016-05-11 12:34:42 -0500

    Adding max of a stack

---
+++ InterviewCakeQuestions/MaxStack.java
@@ -0,0 +1,33 @@
+/**
+ * Use the built-in stack method to create a new class MaxStack with a function
+ * getMax() that returns the largest element in the stack. getMax() should not
+ * remove the item.
+ * Gotchas: this is solvable in O(1) time for push(), pop(), and getMax()
+ */
+
+import java.util.Stack;
+
+public class MaxStack {
+  Stack<Integer> stack = new Stack<Integer>();
+  Stack<Integer> maxsStack = new Stack<Integer>();
+
+  public int push(int element) {
+    stack.push(element);
+    if (maxsStack.empty() || element >= (int) maxsStack.peek()) {
+      maxsStack.push(element);
+    }
+    return element;
+  }
+
+  public int pop() {
+    int element = (int) stack.pop();
+    if (element == (int) maxsStack.peek()) {
+      maxsStack.pop();
+    }
+    return element
+  }
+
+  public int getMax() {
+    return (int) maxsStack.peek();
+  }
+}
