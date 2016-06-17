5bc9fd4eafc221762ca928ca9605eb36815923e0
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2016-06-17 14:32:50 -0500

    Adding preACM into one big repo

---
+++ preACM/src/QuickBrownFox.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class QuickBrownFox {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int caseAmount = input.nextInt();
+		input.nextLine();
+		
+		for (int i = 0; i<caseAmount; i++){
+			String old = input.nextLine();
+			
+			ArrayList<Integer> arr = new ArrayList<Integer>();
+			String sentence = old.toLowerCase();
+			
+			for (int j = 97; j <= 122; j++){
+				String c = Character.toString((char)j);
+				if (!sentence.contains(c)) {
+					arr.add(j);
+				}
+			}
+			
+			if (arr.isEmpty()) {
+				System.out.println("pangram");
+			} else {
+				System.out.print("missing ");
+				for (int j=0; j < arr.size(); j++){
+					int ch = arr.get(j);
+					System.out.print(Character.toString((char) ch));
+				}
+				System.out.println();
+			}
+		}
+		input.close();
+	}
+}
+
