42c075072dc8558b33a05ce80f1b077a20159c89
Emma Sax <emma.the.coder@mail.com>
2016-06-17 14:32:50 -0500

    Adding preACM into one big repo

---
+++ preACM/src/Fibonacci.java
@@ -0,0 +1,26 @@
+public class Fibonacci {
+    public static HashMap<Integer,Integer> prevCalculatedFibs = new HashMap<Integer,Integer>();
+    
+    public static void main (String[] args) {
+        // Some examples of using the method fib()
+        System.out.println("fib(1): " + fib(1)); // should print 1
+        System.out.println("fib(2): " + fib(2)); // should print 1
+        System.out.println("fib(5): " + fib(5)); // should print 5
+        System.out.println("fib(6): " + fib(6)); // should print 8
+    }
+    
+    public static int fib(int input) {
+        if (input == 1 || input == 2) { // these are our base cases
+            return 1;
+        } else {
+            int ans = prevCalculatedFibs.get(input);
+            if (ans != null) { // we have seen this input already
+                return ans;
+            } else { // we haven’t seen this input already
+                int calculated_ans = fib(input - 1) + fib(input - 2);
+                prevCalculatedFibs.put(input, calculated_ans);
+                return calculated_ans;
+            }
+        }
+    }
+}
---
+++ preACM/src/SimonSays.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SimonSays {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int caseAmount = input.nextInt();
+		input.nextLine();
+		
+		String compare = "Simon says";
+		
+		for (int i = 0; i < caseAmount; i++){
+			String sentence = input.nextLine();
+			if (sentence.length() > 10) {
+				if (sentence.substring(0, 10).equals(compare)){
+					System.out.println(sentence.substring(11));
+				}
+			}
+		}
+		
+		input.close();
+	}
+}
+
