8dfc198e782845b5d0c6cd060dffb4c1d094ae1a
Emma Sax <sax.emma.h@gmail.com>
2016-07-22 09:43:45 -0500

    Adding next weekly coding problem

---
+++ InterviewCakeQuestions/RecursiveStringPermutations.java
@@ -0,0 +1,28 @@
+/**
+ * Write a recursive function that generates all permutations of a given string. Return the permutations as a set.
+ * Your function can have loops. It just also must be recursive.
+ */
+
+public class RecursiveStringPermutations {
+  public static Set<String> getPermutations(String input) {
+     Set<String> permutationsSet = new HashSet<String>();
+
+     if (input.length() <= 1) {
+      return new HashSet<String>(Arrays.asList(input));
+     }
+
+     String inputExceptLastLetter = input.substring(0, inputString.length() - 1);
+     char lastLetter = input.charAt(input.length() - 1);
+
+     Set<String> permutationsExceptLastLetter = getPermutations(inputExceptLastLetter);
+
+     for (String permutationSoFar : permutationsExceptLastLetter) {
+      for (int i = 0; i <= permutationSoFar.length(); i++) {
+        String permutation = permutationsSoFar.substring(0, i) + lastLetter + permutationsSoFar.substring(i);
+        permutationsSet.add(permutation);
+      }
+     }
+
+     return permutationsSet;
+  }
+}
