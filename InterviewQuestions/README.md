0c3f18445816b71bdaa463f8f5ad9d05bde9c090
Emma Sax <emma.sax4@gmail.com>
2017-06-20 13:14:28 -0500

    Adding interview questions

---
+++ InterviewQuestions/README.md
@@ -0,0 +1,3 @@
+# InterviewQuestions
+
+Three problems that I had to work on for an interview I had during my senior year of college, in October 2016. Per request from the company, I will not say what company the interview was for. For each problem, I was given thirty minutes to solve all of them. Only one solution is incomplete. In hindsight, I think I should've used a directory object and a file object and created a tree to store the data, but hindsight's 20/20.
---
+++ InterviewQuestions/problem_one.rb
@@ -0,0 +1,31 @@
+# A zero-indexed array A consisting of N integers is given.
+# An equilibrium index of this array is any integer P such that
+# 0 <= P < 0 and the sum of elements of lower indices is equal
+# to the sum of elements of higher indices. Write a method that,
+# given a zero-indexed array A consisting of N integers,
+# returns any of its equilibrium indices. It should return -1 if
+# no equilibrium index exists.
+
+def solution(a)
+  ans = -1
+  left_index, right_index, right_sum, left_sum = nil
+
+  a.each_with_index do |element, index|
+    left_sum = 0
+    right_sum = 0
+
+    for left_index in 0..index-1
+      left_sum += a[left_index]
+    end
+
+    for right_index in index+1..a.length-1
+      right_sum += a[right_index]
+    end
+
+    if left_sum == right_sum
+      ans = index
+    end
+  end
+
+  return ans
+end
